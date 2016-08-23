fe7fd175cf15189a399dc2b5e28abb54ac5d1fff
emiliocorpus <emiliocorpus@gmail.com>
2016-08-22 17:42:58 -0700

    adds search functionality onSubmit

--- client/app/bundles/StatMeQuick/components/NaviAndUI.jsx
+++ client/app/bundles/StatMeQuick/components/NaviAndUI.jsx
@@ -4,13 +4,19 @@ import _ from 'lodash';
 
 export default class NaviAndUI extends React.Component {
 
+	handleSearch(e) {
+		e.preventDefault()
+		debugger
+	}
+
+
 
 	render() {
 		return (
 			<div className="row navigation-ui-container debugger-blue">
-				<form className="navbar-form navbar-left" role="search">
+				<form className="navbar-form navbar-left" role="search" onSubmit={this.handleSearch}>
 				  <div className="form-group">
-				    <input type="text" className="form-control" placeholder="Search"/>
+				    <input type="text" className="form-control" placeholder="Search" />
 				  </div>
 				  <button type="submit" className="btn btn-default">Submit</button>
 				</form>
@@ -18,3 +24,7 @@ export default class NaviAndUI extends React.Component {
 		)
 	}
 }
+
+
+// things to do,
+// 1. Seed database with names of every player.
