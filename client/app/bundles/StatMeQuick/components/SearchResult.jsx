24c2d6110a16d96646f9f18c7e9dfd7aebcea9d3
emiliocorpus <emiliocorpus@gmail.com>
2016-08-23 11:46:21 -0700

    creates new SearchResult component

---
+++ client/app/bundles/StatMeQuick/components/SearchResult.jsx
@@ -0,0 +1,15 @@
+import React, { PropTypes } from 'react';
+import _ from 'lodash';
+import ReactFlipMove from 'react-flip-move';
+
+export default class SearchResult extends React.Component {
+	render() {
+		return (
+			<div className="row">
+
+				This is Search Result
+
+			</div>
+		)
+	}
+}
