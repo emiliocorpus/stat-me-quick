458d7f1ed43f1ad3d9342f3b19708f1374b0c03e
emiliocorpus <emiliocorpus@gmail.com>
2016-08-22 17:50:23 -0700

    adds model for Player name

---
+++ db/migrate/20160823005019_create_players.rb
@@ -0,0 +1,11 @@
+class CreatePlayers < ActiveRecord::Migration
+  def change
+    create_table :players do |t|
+      t.string :full_name
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ spec/factories/players.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :player do
+    full_name "MyString"
+    first_name "MyString"
+    last_name "MyString"
+  end
+end
