8b099199149275424464945347e158f64b1718bb
emiliocorpus <emiliocorpus@gmail.com>
2016-08-23 11:29:31 -0700

    adds ajax request for searching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,19 @@
 class SearchController < ApplicationController
   def find
+  	search = params["searchValue"]
+  	@result = Player.where('full_name ~* :pat', :pat => search)
+  	if request.xhr?
+        render :json => {:result => @result }
+  	else
+  		redirect_to root_path
+ 	end
   end
 
   def view
+
+
+
+
+
   end
 end
