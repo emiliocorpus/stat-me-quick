bc6900d5e3fe77b77ce7c0043c8372c82b9c90ea
emiliocorpus <emiliocorpus@gmail.com>
2016-08-23 14:29:20 -0700

    adds ui for searching

--- client/app/bundles/StatMeQuick/components/SearchResult.jsx
+++ client/app/bundles/StatMeQuick/components/SearchResult.jsx
@@ -1,13 +1,41 @@
 import React, { PropTypes } from 'react';
 import _ from 'lodash';
-import ReactFlipMove from 'react-flip-move';
+import FlipMove from 'react-flip-move';
+import Result from './Result';
 
 export default class SearchResult extends React.Component {
-	render() {
-		return (
-			<div className="row">
 
-				This is Search Result
+	constructor(props) {
+	  super(props);
+
+		this.createResults = this.createResults.bind(this)
+	}
+
+	createResults() {
+		var searchResults = [];
+		for (var i=0; i < this.props.data.length;i++) {
+			searchResults.push(<Result key={i} data={this.props.data[i]}/>)
+		}
+		return searchResults
+	}
+
+
+	render() {
+		var searchComment;
+		if (this.props.data.length == 1) {
+			searchComment = "1 match found."
+		}
+		else {
+			searchComment = this.props.data.length.toString() + " matches found."
+		}
+
+		return (
+			<div className="row ">
+				<span className="search-comment">{searchComment}</span>
+
+				<div className="container-fluid result-tray">
+						{this.createResults()}
+				</div>
 
 			</div>
 		)
