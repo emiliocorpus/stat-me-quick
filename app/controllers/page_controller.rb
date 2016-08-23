4cffc5b453798063c79149cb5cdc9b531c70b602
emiliocorpus <emiliocorpus@gmail.com>
2016-08-22 18:14:06 -0700

    updates controller, debugging for Nokogiri parsing

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,4 +1,33 @@
+require 'open-uri'
+
 class PageController < ApplicationController
   def index
+
+
+
+  	# http://www.foxsports.com/nfl/players
+  	i = 1
+  	# while i < 34
+
+  		url = "http://www.foxsports.com/nfl/players?teamId=#{i}&season=2016&position=0&name="
+
+
+  		page = Nokogiri::HTML(open(url))
+  		tbody = page.css("tbody")
+  		players = tbody.css("tr")
+  		binding.pry
+
+
+
+
+
+
+
+
+  		i+=1
+  	# end
+
+
+
   end
 end
